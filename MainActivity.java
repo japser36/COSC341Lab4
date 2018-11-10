17926f7ff5112ab81571dcf75599d7f5921e3208
japser36 <jasper@looman.ca>
2018-11-10 10:28:37 -0800

    Finished MainActivity

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.a42305169.cosc341lab4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void readData(View view) {
+        File f = new File("@string/dataFile");
+        Intent i = null;
+        if (f.exists() && !f.isDirectory()) {
+            i = new Intent(this, ReadActivity.class);
+        } else {
+            Toast.makeText(getApplicationContext(), "@string/writeFirst", Toast.LENGTH_SHORT).show();
+        }
+        startActivity(i);
+    }
+
+    public void writeData(View view) {
+        Intent i = new Intent(this, WriteActivity.class);
+        startActivity(i);
     }
 }
